0c139a2c7dc7012048ff520b05a50b9334e2c8cd
Andrey Kirillov <kirillov.andrey.123.15@gmail.com>
2023-11-11 15:01:13 +0300

    add lab matrix

--- include/tmatrix.h
+++ include/tmatrix.h
@@ -20,103 +20,193 @@ template<typename T>
 class TDynamicVector
 {
 protected:
-  size_t sz;
-  T* pMem;
+    size_t sz;
+    T* pMem;
 public:
-  TDynamicVector(size_t size = 1) : sz(size)
-  {
-    if (sz == 0)
-      throw out_of_range("Vector size should be greater than zero");
-    pMem = new T[sz]();// {}; // У типа T д.б. констуктор по умолчанию
-  }
-  TDynamicVector(T* arr, size_t s) : sz(s)
-  {
-    assert(arr != nullptr && "TDynamicVector ctor requires non-nullptr arg");
-    pMem = new T[sz];
-    std::copy(arr, arr + sz, pMem);
-  }
-  TDynamicVector(const TDynamicVector& v)
-  {
-  }
-  TDynamicVector(TDynamicVector&& v) noexcept
-  {
-  }
-  ~TDynamicVector()
-  {
-  }
-  TDynamicVector& operator=(const TDynamicVector& v)
-  {
-  }
-  TDynamicVector& operator=(TDynamicVector&& v) noexcept
-  {
-  }
+    TDynamicVector(size_t size = 1) : sz(size)
+    {
+        if (sz <= 0 || sz > MAX_VECTOR_SIZE){
+            throw out_of_range("Vector size should be greater than zero and less than max_vector_size");
+        }
+        pMem = new T[sz]();// {}; // У типа T д.б. констуктор по умолчанию
+    }
+    TDynamicVector(T* arr, size_t s) : sz(s)
+    {
+        if (sz <= 0 || sz > MAX_VECTOR_SIZE) {
+            throw out_of_range("Vector size should be greater than zero and less than max_vector_size");
+        }
+        assert(arr != nullptr && "TDynamicVector ctor requires non-nullptr arg");
+        pMem = new T[sz];
+        std::copy(arr, arr + sz, pMem);
+    }
+    TDynamicVector(const TDynamicVector& v)
+    {
+        sz = v.sz;
+        pMem = new T[sz];
+        std::copy(v.pMem, v.pMem + sz, pMem);
+    }
+    TDynamicVector(TDynamicVector&& v) noexcept
+    {
+        pMem = nullptr;
+        sz = 0;
+        swap(*this, v);
+    }
+    ~TDynamicVector()
+    {
+        delete[] pMem;
+        sz = 0;
+    }
+    TDynamicVector& operator=(const TDynamicVector& v)
+    {
+        if (*this == v) {
+            return *this;
+        }
+        if (sz != v.sz) {
+            T* p = new T[v.sz];
+            delete[] pMem;
+            sz = v.sz;
+            pMem = p;
+        }
+        std::copy(v.pMem, v.pMem + sz, pMem);
+        return *this;
+    }
+    TDynamicVector& operator=(TDynamicVector&& v) noexcept
+    {
+        delete[] pMem;
+        pMem = nullptr;
+        sz = 0;
+        swap(*this, v);
+        return *this;
+    }
 
-  size_t size() const noexcept { return sz; }
+    size_t size() const noexcept { return sz; }
 
-  // индексация
-  T& operator[](size_t ind)
-  {
-  }
-  const T& operator[](size_t ind) const
-  {
-  }
-  // индексация с контролем
-  T& at(size_t ind)
-  {
-  }
-  const T& at(size_t ind) const
-  {
-  }
+    // индексация
+    T& operator[](size_t ind)
+    {
+        return pMem[ind];
+    }
+    const T& operator[](size_t ind) const
+    {
+        return pMem[ind];
+    }
+    // индексация с контролем
+    T& at(size_t ind)
+    {
+        if (ind < sz && ind >= 0) {
+            return pMem[ind];
+        }
+        throw out_of_range("out of range");
+    }
+    const T& at(size_t ind) const
+    {
+        if (ind < sz && ind >= 0) {
+            return pMem[ind];
+        }
+        throw out_of_range("out of range");
+    }
 
-  // сравнение
-  bool operator==(const TDynamicVector& v) const noexcept
-  {
-  }
-  bool operator!=(const TDynamicVector& v) const noexcept
-  {
-  }
+    // сравнение
+    bool operator==(const TDynamicVector& v) const noexcept
+    {
+        if (sz != v.sz) {
+            return false;
+        }
+        for (size_t i = 0; i < sz; i++) {
+            if (pMem[i] != v.pMem[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool operator!=(const TDynamicVector& v) const noexcept
+    {
+        if (*this == v) {
+            return false;
+        }
+        return true;
+    }
 
-  // скалярные операции
-  TDynamicVector operator+(T val)
-  {
-  }
-  TDynamicVector operator-(double val)
-  {
-  }
-  TDynamicVector operator*(double val)
-  {
-  }
+    // скалярные операции
+    TDynamicVector operator+(T val)
+    {
+        TDynamicVector tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] + val;
+        }
+        return tmp;
+    }
+    TDynamicVector operator-(T val)
+    {
+        TDynamicVector tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] - val;
+        }
+        return tmp;
+    }
+    TDynamicVector operator*(T val)
+    {
+        TDynamicVector tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] * val;
+        }
+        return tmp;
+    }
 
-  // векторные операции
-  TDynamicVector operator+(const TDynamicVector& v)
-  {
-  }
-  TDynamicVector operator-(const TDynamicVector& v)
-  {
-  }
-  T operator*(const TDynamicVector& v) noexcept(noexcept(T()))
-  {
-  }
+    // векторные операции
+    TDynamicVector operator+(const TDynamicVector& v)
+    {
+        if (sz != v.sz) {
+            throw out_of_range("not equal sizes");
+        }
+        TDynamicVector tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] + v.pMem[i];
+        }
+        return tmp;
+    }
+    TDynamicVector operator-(const TDynamicVector& v)
+    {
+        if (sz != v.sz) {
+            throw out_of_range("not equal sizes");
+        }
+        TDynamicVector tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] - v.pMem[i];
+        }
+        return tmp;
+    }
+    T operator*(const TDynamicVector& v) //noexcept(noexcept(T()))
+    {
+        if (sz != v.sz) {
+            throw out_of_range("not equal sizes");
+        }
+        T tmp = 0;
+        for (size_t i = 0; i < sz; i++) {
+            tmp += pMem[i] * v.pMem[i];
+        }
+        return tmp;
+    }
 
-  friend void swap(TDynamicVector& lhs, TDynamicVector& rhs) noexcept
-  {
-    std::swap(lhs.sz, rhs.sz);
-    std::swap(lhs.pMem, rhs.pMem);
-  }
+    friend void swap(TDynamicVector& lhs, TDynamicVector& rhs) noexcept
+    {
+        std::swap(lhs.sz, rhs.sz);
+        std::swap(lhs.pMem, rhs.pMem);
+    }
 
-  // ввод/вывод
-  friend istream& operator>>(istream& istr, TDynamicVector& v)
-  {
-    for (size_t i = 0; i < v.sz; i++)
-      istr >> v.pMem[i]; // требуется оператор>> для типа T
-    return istr;
-  }
-  friend ostream& operator<<(ostream& ostr, const TDynamicVector& v)
-  {
-    for (size_t i = 0; i < v.sz; i++)
-      ostr << v.pMem[i] << ' '; // требуется оператор<< для типа T
-    return ostr;
-  }
+    // ввод/вывод
+    friend istream& operator>>(istream& istr, TDynamicVector& v)
+    {
+        for (size_t i = 0; i < v.sz; i++)
+            istr >> v.pMem[i]; // требуется оператор>> для типа T
+        return istr;
+    }
+    friend ostream& operator<<(ostream& ostr, const TDynamicVector& v)
+    {
+        for (size_t i = 0; i < v.sz; i++)
+            ostr << v.pMem[i] << ' '; // требуется оператор<< для типа T
+        return ostr;
+    }
 };
 
 
@@ -125,50 +215,116 @@ public:
 template<typename T>
 class TDynamicMatrix : private TDynamicVector<TDynamicVector<T>>
 {
-  using TDynamicVector<TDynamicVector<T>>::pMem;
-  using TDynamicVector<TDynamicVector<T>>::sz;
+    using TDynamicVector<TDynamicVector<T>>::pMem;
+    using TDynamicVector<TDynamicVector<T>>::sz;
 public:
-  TDynamicMatrix(size_t s = 1) : TDynamicVector<TDynamicVector<T>>(s)
-  {
-    for (size_t i = 0; i < sz; i++)
-      pMem[i] = TDynamicVector<T>(sz);
-  }
+    TDynamicMatrix(size_t s = 1) : TDynamicVector<TDynamicVector<T>>(s)
+    {
+        if(sz <= 0 || sz > MAX_MATRIX_SIZE){
+            throw out_of_range("size should be greater than zero and less max matrix size");
+        }
+        for (size_t i = 0; i < sz; i++)
+            pMem[i] = TDynamicVector<T>(sz);
+    }
 
-  using TDynamicVector<TDynamicVector<T>>::operator[];
+    using TDynamicVector<TDynamicVector<T>>::operator[];
 
-  // сравнение
-  bool operator==(const TDynamicMatrix& m) const noexcept
-  {
-  }
+    T& at(size_t ind1, size_t ind2)
+    {
+        if ((ind1 < sz && ind1 >= 0) && (ind2 < sz && ind2 >= 0))
+            return pMem[ind1][ind2];
+        else
+            throw out_of_range("indices should be greater than zero and less max matrix size");
+    }
 
-  // матрично-скалярные операции
-  TDynamicVector<T> operator*(const T& val)
-  {
-  }
+    // сравнение
+    bool operator==(const TDynamicMatrix& m) const noexcept
+    {
+        if (sz != m.sz) {
+            return false;
+        }
+        return TDynamicVector<TDynamicVector<T>>::operator==(m);
+    }
 
-  // матрично-векторные операции
-  TDynamicVector<T> operator*(const TDynamicVector<T>& v)
-  {
-  }
+    size_t size() const noexcept { return sz; }
 
-  // матрично-матричные операции
-  TDynamicMatrix operator+(const TDynamicMatrix& m)
-  {
-  }
-  TDynamicMatrix operator-(const TDynamicMatrix& m)
-  {
-  }
-  TDynamicMatrix operator*(const TDynamicMatrix& m)
-  {
-  }
+    // матрично-скалярные операции
+    TDynamicMatrix<T> operator*(const T& val)
+    {
+        TDynamicMatrix tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] * val;
+        }
+        return tmp;
+    }
 
-  // ввод/вывод
-  friend istream& operator>>(istream& istr, TDynamicMatrix& v)
-  {
-  }
-  friend ostream& operator<<(ostream& ostr, const TDynamicMatrix& v)
-  {
-  }
+    // матрично-векторные операции
+    TDynamicVector<T> operator*(const TDynamicVector<T>& v)
+    {
+        if (sz != v.size()) {
+            throw logic_error("sizes matrix and vector should be equal");
+        }
+        TDynamicVector<T> tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp[i] = pMem[i] * v;
+        }
+        return tmp;
+    }
+
+    // матрично-матричные операции
+    TDynamicMatrix operator+(const TDynamicMatrix& m)
+    {
+        if (sz != m.sz) {
+            throw logic_error("sizes matrices should be equal");
+        }
+        TDynamicMatrix<T> tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] + m.pMem[i];
+        }
+        return tmp;
+    }
+    TDynamicMatrix operator-(const TDynamicMatrix& m)
+    {
+        if (sz != m.sz) {
+            throw logic_error("sizes matrices should be equal");
+        }
+        TDynamicMatrix<T> tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            tmp.pMem[i] = pMem[i] - m.pMem[i];
+        }
+        return tmp;
+    }
+    TDynamicMatrix operator*(const TDynamicMatrix& m)
+    {
+        if (sz != m.sz) {
+            throw logic_error("sizes matrices should be equal");
+        }
+        TDynamicMatrix<T> tmp(sz);
+        for (size_t i = 0; i < sz; i++) {
+            for (size_t j = 0; j < sz; j++) {
+                for (size_t k = 0; k < sz; k++) {
+                    tmp.pMem[i][j] += pMem[i][k] * m.pMem[k][j];
+                }
+            }
+        }
+        return tmp;
+    }
+
+    // ввод/вывод
+    friend istream& operator>>(istream& istr, TDynamicMatrix& v)
+    {
+        for (size_t i = 0; i < v.sz; i++) {
+            istr >> v[i];
+        }
+        return istr;
+    }
+    friend ostream& operator<<(ostream& ostr, const TDynamicMatrix& v)
+    {
+        for (size_t i = 0; i < v.sz; i++) {
+            ostr << v[i] << endl;
+        }
+        return ostr;
+    }
 };
 
 #endif
--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -8,7 +8,7 @@
 #include "tmatrix.h"
 //---------------------------------------------------------------------------
 
-void main()
+void nmain()
 {
   TDynamicMatrix<int> a(5), b(5), c(5);
   int i, j;
@@ -28,3 +28,303 @@ void main()
   cout << "Matrix c = a + b" << endl << c << endl;
 }
 //---------------------------------------------------------------------------
+
+
+template <class T>
+void rand_vector(TDynamicVector<T>& vec);
+
+template <class T>
+void rand_matrix(TDynamicMatrix<T>& mat, size_t size_matrix);
+
+template <class T>
+void create_vector(TDynamicVector<T>& v, size_t size_vector);
+
+template <class T>
+void create_matrix(TDynamicMatrix<T>& mat, size_t size_matrix);
+
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int choice;
+    size_t size_vector_a, size_vector_b, size_matrix_A, size_matrix_B;
+    TDynamicVector<double> vector_a, vector_b;
+    TDynamicMatrix<double> matrix_A, matrix_B;
+    double alpha;
+    
+    while(1)
+    {
+        cout << "/----------------------------------------------------------------/" << endl;
+        cout << " МЕНЮ:" << endl;
+        cout << " 0 -- a+b векторная операция" << endl;
+        cout << " 1 -- a-b векторная операция" << endl;
+        cout << " 2 -- a*b векторная операция (a,b)" << endl;
+        cout << " 3 -- a+alpha векторно-скалярня операция" << endl;
+        cout << " 4 -- a-alpha векторно-скалярня операция" << endl;
+        cout << " 5 -- a*alpha векторно-скалярня операция" << endl;
+        cout << " 6 -- A*alpha матрично-скалярня операция" << endl;
+        cout << " 7 -- A*b матрично-векторная операция" << endl;
+        cout << " 8 -- A+B матрично-матричная операция" << endl;
+        cout << " 9 -- A-B матрично-матричная операция" << endl;
+        cout << " 10 -- A*B матрично-матричная операция" << endl;
+        cout << " 11 -- END " << endl;
+        cout << " Выбор операции: ";
+        cin >> choice;
+        cout << endl;
+        switch (choice)
+        {
+        case 0: {
+            try
+            {
+                cout << " Введите размер a: ";
+                cin >> size_vector_a;
+                create_vector(vector_a, size_vector_a);
+                cout << " Введите размер b: ";
+                cin >> size_vector_b;
+                create_vector(vector_b, size_vector_b);
+                cout << " a+b=" << endl << vector_a + vector_b << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }           
+            break;
+        }
+        case 1: {
+            try
+            {
+                cout << " Введите размер a: ";
+                cin >> size_vector_a;
+                create_vector(vector_a, size_vector_a);
+                cout << " Введите размер b: ";
+                cin >> size_vector_b;
+                create_vector(vector_b, size_vector_b);
+                cout << " a-b=" << endl << vector_a - vector_b << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 2: {
+            try
+            {
+                cout << " Введите размер a: ";
+                cin >> size_vector_a;
+                create_vector(vector_a, size_vector_a);
+                cout << " Введите размер b: ";
+                cin >> size_vector_b;
+                create_vector(vector_b, size_vector_b);
+                cout << " a*b=" << endl << vector_a * vector_b << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 3: {
+            try
+            {
+                cout << " Введите размер a: ";
+                cin >> size_vector_a;
+                create_vector(vector_a, size_vector_a);
+                cout << " Введите alpha: ";
+                cin >> alpha;
+                cout << " a+alpha=" << endl << vector_a + alpha << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 4: {
+            try
+            {
+                cout << " Введите размер a: ";
+                cin >> size_vector_a;
+                create_vector(vector_a, size_vector_a);
+                cout << " Введите alpha: ";
+                cin >> alpha;
+                cout << " a-alpha=" << endl << vector_a - alpha << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 5: {
+            try
+            {
+                cout << " Введите размер a: ";
+                cin >> size_vector_a;
+                create_vector(vector_a, size_vector_a);
+                cout << " Введите alpha: ";
+                cin >> alpha;
+                cout << " a*alpha=" << endl << vector_a * alpha << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 6: {
+            try
+            {
+                cout << " Введите размер A: ";
+                cin >> size_matrix_A;
+                create_matrix(matrix_A, size_matrix_A);
+                cout << " Введите alpha: ";
+                cin >> alpha;
+                cout << " a*alpha=" << endl << matrix_A * alpha << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 7: {
+            try
+            {
+                cout << " Введите размер A: ";
+                cin >> size_matrix_A;
+                create_matrix(matrix_A, size_matrix_A);
+                cout << " Введите размер b: ";
+                cin >> size_vector_b;
+                create_vector(vector_b, size_vector_b);
+                cout << " A*b=" << endl << matrix_A * vector_b << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 8: {
+            try
+            {
+                cout << " Введите размер A: ";
+                cin >> size_matrix_A;
+                create_matrix(matrix_A, size_matrix_A);
+                cout << " Введите размер B: ";
+                cin >> size_matrix_B;
+                create_matrix(matrix_B, size_matrix_B);
+                cout << " A+B=" << endl << matrix_A + matrix_B << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 9: {
+            try
+            {
+                cout << " Введите размер A: ";
+                cin >> size_matrix_A;
+                create_matrix(matrix_A, size_matrix_A);
+                cout << " Введите размер B: ";
+                cin >> size_matrix_B;
+                create_matrix(matrix_B, size_matrix_B);
+                cout << " A-B=" << endl << matrix_A - matrix_B << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 10: {
+            try
+            {
+                cout << " Введите размер A: ";
+                cin >> size_matrix_A;
+                create_matrix(matrix_A, size_matrix_A);
+                cout << " Введите размер B: ";
+                cin >> size_matrix_B;
+                create_matrix(matrix_B, size_matrix_B);
+                cout << " A*B=" << endl << matrix_A * matrix_B << endl;
+            }
+            catch (const std::exception& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        }
+        case 11: {
+            cout << "/----------------------------------------------------------------/" << endl<<endl;
+            cout << "Программа завершена!" << endl;
+            return 0;
+        }
+        default: {
+            cout << endl << "Неправильный выбор!" << endl << endl;
+            break;
+        }
+        }
+    }
+}
+
+
+
+
+template <class T>
+void rand_vector(TDynamicVector<T>& vec) {
+    for (size_t i = 0; i < vec.size(); i++) {
+        vec[i] = ((T)rand()) / 100;
+    }
+}
+
+template <class T>
+void rand_matrix(TDynamicMatrix<T>& mat, size_t size_matrix) {
+    for (size_t i = 0; i < size_matrix; i++) {
+        for (size_t j = 0; j < size_matrix; j++) {
+            mat[i][j] = ((T)rand()) / 100;
+        }
+    }
+}
+template <class T>
+void create_vector(TDynamicVector<T>& v, size_t size_vector) {
+    TDynamicVector<T> vec(size_vector);
+    int choice_rand;
+    cout << " 1 -- Рандомное заполнение" << endl;
+    cout << " 2 -- Ручное заполнение" << endl;
+    cout << " Выбор: ";
+    cin >> choice_rand;
+    cout << endl;
+    if (choice_rand == 1) {
+        rand_vector(vec);
+        cout << " vec=" << endl << vec << endl;
+    }
+    else {
+        cout << " Заполнение вектора" << endl;
+        cin >> vec;
+        cout << " a=" << endl << vec << endl;
+    }
+    v = vec;
+}
+
+template <class T>
+void create_matrix(TDynamicMatrix<T>& mat, size_t size_matrix) {
+    TDynamicMatrix<T> mt(size_matrix);
+    int choice_rand;
+    cout << " 1 -- Рандомное заполнение" << endl;
+    cout << " 2 -- Ручное заполнение" << endl;
+    cout << " Выбор: ";
+    cin >> choice_rand;
+    cout << endl;
+    if (choice_rand == 1) {
+        rand_matrix(mt, size_matrix);
+        cout << " matr=" << endl << mt << endl;
+    }
+    else {
+        cout << " Заполнение вектора" << endl;
+        cin >> mt;
+        cout << " matr=" << endl << mt << endl;
+    }
+    mat = mt;
+}
