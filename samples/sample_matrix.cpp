5ad85a0402c88d11db6fb055b43010eb6a8ea882
IljarSmirnov <ilsmirnov693n@gmail.com>
2023-11-12 13:29:11 +0300

    Update sample_matrix.cpp

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -6,25 +6,47 @@
 
 #include <iostream>
 #include "tmatrix.h"
+
+//---------------------------------------------------------------------------
+
+#include <iostream>
+#include "tmatrix.h"
 //---------------------------------------------------------------------------
 
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
+   
+    size_t sz,ch2;
+    char ch;
+    cout << "Enter matrix's sizes" << endl;
+    cin >> sz;
+    TDynamicMatrix<int> a(sz), b(sz), c(sz);
+    cout << "Enter first matrix" << endl;
+    cin >> a;
+    cout << "Enter second matrix" << endl;
+    cin >> b;
+    cout << "Enter the operation + for matrix addition, - for subtraction, * for multiplication" << std::endl;
+    do {
+        cin >> ch;
+        if ((ch != '+') && (ch != '-') && (ch != '*')) {
+            std::cout << "Wrong input. Try again." << std::endl;
+        }
+        else if (ch == '-') {
+            cout << "Enter 0 if you want to subtract the second one from the first matrix, and 1 otherwise " << endl;
+            do {
+                cin >> ch2;
+            } while (ch2 != 0 && ch2 != 1);
+        }
+    } while ((ch != '+') && (ch != '-') && (ch != '*'));
+    if (ch == '+') c = a + b;
+    if (ch == '-') {
+        if (ch2 == 0) c = a - b;
+        else c = b - a;
     }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+    if (ch == '*') c = a * b;
+
+    cout << "Matrix a = " << endl << a << endl;
+    cout << "Matrix b = " << endl << b << endl;
+    cout << "Matrix c = a" << ch<< "b" << endl << c << endl;
 }
 //---------------------------------------------------------------------------
