472acf9c8bd4aca0c0dd182ebad6e42b0c17565f
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-11-13 21:48:28 +0300

    Add files via upload

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -1,30 +1,85 @@
-﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+﻿// основа.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-// Copyright (c) Сысоев А.В.
-//
-// Тестирование матриц
 
 #include <iostream>
 #include "tmatrix.h"
-//---------------------------------------------------------------------------
 
-void main()
+int main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
+    setlocale(LC_ALL, "Russian");
+    cout << "Тестирование класса работы с матрицами" << endl;
+    int n;
+    cout << "Выберите операцию:" << endl;
+    cout << "1. Сумма двух матриц" << endl;
+    cout << "2. Разность двух матриц" << endl;
+    cout << "3. Умножение двух матриц" << endl;
+    cout << "4. Умножение матрицы на вектор" << endl;
+    cout << "5. Умножение матрицы на константу" << endl;
+    cin >> n;
+    size_t size = 0;
+    if (n == 1)
     {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
+        cout << "Введите размер матриц:" << endl;
+        cin >> size;
+        TDynamicMatrix<double> A(size);
+        cout << "Введите элементы первой матрицы:" << endl;
+        cin >> A;
+        TDynamicMatrix<double> B(size);
+        cout << "Введите элементы второй матрицы:" << endl;
+        cin >> B;
+        cout << "Результат:" << endl;
+        cout << A + B << endl;
     }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+    if (n == 2)
+    {
+        cout << "Введите размер матриц:" << endl;
+        cin >> size;
+        TDynamicMatrix<double> A(size);
+        cout << "Введите элементы первой матрицы:" << endl;
+        cin >> A;
+        TDynamicMatrix<double> B(size);
+        cout << "Введите элементы второй матрицы:" << endl;
+        cin >> B;
+        cout << "Результат:" << endl;
+        cout << A - B << endl;
+    }
+    if (n == 3)
+    {
+        cout << "Введите размер матриц:" << endl;
+        cin >> size;
+        TDynamicMatrix<double> A(size);
+        cout << "Введите элементы первой матрицы:" << endl;
+        cin >> A;
+        TDynamicMatrix<double> B(size);
+        cout << "Введите элементы второй матрицы:" << endl;
+        cin >> B;
+        cout << "Результат:" << endl;
+        cout << A * B << endl;
+    }
+    if (n == 4)
+    {
+        cout << "Введите размер матрицы и вектора:" << endl;
+        cin >> size;
+        TDynamicMatrix<double> A(size);
+        cout << "Введите элементы матрицы:" << endl;
+        cin >> A;
+        TDynamicVector<double> b(size);
+        cout << "Введите элементы вектора:" << endl;
+        cin >> b;
+        cout << "Результат:" << endl;
+        cout << A * b << endl;
+    }
+    if (n == 5)
+    {
+        cout << "Введите размер матрицы" << endl;
+        cin >> size;
+        TDynamicMatrix<double> A(size);
+        cout << "Введите элементы матрицы:" << endl;
+        cin >> A;
+        double constanta = 0;
+        cout << "Введите константу:" << endl;
+        cin >> constanta;
+        cout << "Результат:" << endl;
+        cout << A * constanta << endl;
+    }
 }
-//---------------------------------------------------------------------------
