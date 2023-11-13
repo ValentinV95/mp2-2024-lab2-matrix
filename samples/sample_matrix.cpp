a9c5f1313c1aa921ac8607bfafbe948a60f221d4
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2023-11-13 21:30:16 +0300

    Add files via upload

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -14,198 +14,18 @@ int main()
     int n = 10;
     while (1)
     {
-        cout << "1 - V + scalar" << endl;
-        cout << "2 - V - scalar" << endl;
-        cout << "3 - V * scalar" << endl;
-        cout << "4 - V1 + V2" << endl;
-        cout << "5 - V1 - V2" << endl;
-        cout << "6 - (V1, V2)" << endl;
-        cout << "7 - M * scalar" << endl;
-        cout << "8 - M * V" << endl;
-        cout << "9 - M1 + M2" << endl;
-        cout << "10 - M1 - M2" << endl;
-        cout << "11 - M1 * M2" << endl;
+        cout << "1 - M * scalar" << endl;
+        cout << "2 - M * V" << endl;
+        cout << "3 - M1 + M2" << endl;
+        cout << "4 - M1 - M2" << endl;
+        cout << "5 - M1 * M2" << endl;
         cout << "0 - exit" << endl;
 
         cin >> n;
 
         switch (n)
         {
-            case(1): //V + scalar
-            {
-                try{
-                size_t s;
-                double scalar;
-
-                cout << "Enter size V:" << endl;
-                cin >> s;
-                TDynamicVector<double> V(s);
-
-                cout << "Fill V:" << endl;
-                cin >> V;
-
-                cout << "Enter scalar:" << endl;
-                cin >> scalar;
-
-                cout << "Answer:" << endl;
-                cout << (V + scalar) << endl;
-                cout << "_________" << endl;
-            }
-                catch (exception& e) {
-                    cout << "error: " << e.what() << endl;
-                    break;
-                }
-                break;
-            }
-
-            case(2): //V - scalar
-            {
-                try{
-                    size_t s;
-                    double scalar;
-
-                    cout << "Enter size V:" << endl;
-                    cin >> s;
-                    TDynamicVector<double> V(s);
-
-                    cout << "Fill V:" << endl;
-                    cin >> V;
-
-                    cout << "Enter scalar:" << endl;
-                    cin >> scalar;
-
-                    cout << "Answer:" << endl;
-                    cout << (V - scalar) << endl;
-                    cout << "_________" << endl;
-                }
-                catch (exception& e) {
-                    cout << "error: " << e.what() << endl;
-                    break;
-                }
-                break;
-            }
-
-            case(3): //V * scalar
-            {
-                try{
-                    size_t s;
-                    double scalar;
-
-                    cout << "Enter size V:" << endl;
-                    cin >> s;
-                    TDynamicVector<double> V(s);
-
-                    cout << "Fill V:" << endl;
-                    cin >> V;
-
-                    cout << "Enter scalar:" << endl;
-                    cin >> scalar;
-
-                    cout << "Answer:" << endl;
-                    cout << (V * scalar) << endl;
-                    cout << "_________" << endl;
-                }
-                catch (exception& e) {
-                    cout << "error: " << e.what() << endl;
-                    break;
-                }
-                break;
-            }
-
-            case(4): //V1 + V2
-            {
-                try {
-                    size_t s1, s2;
-
-                    cout << "Enter size V1" << endl;
-                    cin >> s1;
-                    TDynamicVector<double> V1(s1);
-
-                    cout << "Fill V1:" << endl;
-                    cin >> V1;
-
-
-                    cout << "Enter size V2" << endl;
-                    cin >>s2 ;
-                    TDynamicVector<double> V2(s2);
-
-                    cout << "Fill V2:" << endl;
-                    cin >> V2;
-
-                    cout << "Answer:" << endl;
-                    cout << V1+V2 << endl;
-                    cout << "_________" << endl;
-                }
-                catch (exception& e) {
-                    cout << "error: " << e.what() << endl;
-                    break;
-                }
-                break;
-            }
-
-            case(5): //V1 - V2
-            {
-                try {
-                    size_t s1, s2;
-
-                    cout << "Enter size V1" << endl;
-                    cin >> s1;
-                    TDynamicVector<double> V1(s1);
-
-                    cout << "Fill V1:" << endl;
-                    cin >> V1;
-
-
-                    cout << "Enter size V2" << endl;
-                    cin >>s2 ;
-                    TDynamicVector<double> V2(s2);
-
-                    cout << "Fill V2:" << endl;
-                    cin >> V2;
-
-                    cout << "Answer:" << endl;
-                    cout << V1-V2 << endl;
-                    cout << "_________" << endl;
-                }
-                catch (exception& e) {
-                    cout << "error: " << e.what() << endl;
-                    break;
-                }
-                break;
-            }
-
-            case(6): //(V1, V2)
-            {
-                try {
-                    size_t s1, s2;
-
-                    cout << "Enter size V1" << endl;
-                    cin >> s1;
-                    TDynamicVector<double> V1(s1);
-
-                    cout << "Fill V1:" << endl;
-                    cin >> V1;
-
-
-                    cout << "Enter size V2" << endl;
-                    cin >>s2 ;
-                    TDynamicVector<double> V2(s2);
-
-                    cout << "Fill V2:" << endl;
-                    cin >> V2;
-
-                    cout << "Answer:" << endl;
-                    cout << V1*V2 << endl;
-                    cout << "_________" << endl;
-                }
-                catch (exception& e) {
-                    cout << "error: " << e.what() << endl;
-                    break;
-                }
-                break;
-            }
-
-            case(7): //M * scalar
+            case(1): //M * scalar
             {
                 try{
                     size_t s;
@@ -232,7 +52,7 @@ int main()
                 break;
             }
 
-            case(8): //M*V
+            case(2): //M*V
             {
                 try {
                     size_t s1, s2;
@@ -263,7 +83,7 @@ int main()
                 break;
             }
 
-            case(9): //M1 + M2
+            case(3): //M1 + M2
             {
                 try {
                     size_t s1, s2;
@@ -294,7 +114,7 @@ int main()
                 break;
             }
 
-            case(10): //M1 - M2
+            case(4): //M1 - M2
             {
                 try {
                     size_t s1, s2;
@@ -325,7 +145,7 @@ int main()
                 break;
             }
 
-            case(11): //M1*M2
+            case(5): //M1*M2
             {
                 try {
                     size_t s1, s2;
